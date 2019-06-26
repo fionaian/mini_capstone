be549c9952847fa937a2ffb07fc443a8f4a96839
Fiona Ian <sayhitofiona@gmail.com>
2019-06-25 19:23:57 -0500

    seeds file with list of products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "La Croix Mango", price: 4, image_url: "https://i.ebayimg.com/images/g/0u0AAOSwfPlb2X0~/s-l300.jpg", description: "mango flavored sparkling water")
+Product.create(name: "Nutella", price: 10, image_url: "https://www.nutella.com/nutella-gl-theme/images/custom/Nutella.png", description: "hazelnut spread")
+Product.create(name: "Garrett Popcorn", price: 8, image_url: "https://cfcdn2.azsg.opensnap.com/azsg/snapphoto/photo/LB/GUFF/3BSDO2247D61C10C31F685lv.jpg", description: "caramel flavored popcorn")
