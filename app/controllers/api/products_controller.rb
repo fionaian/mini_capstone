cbe95665b575222fb493321ae6054f1b8ae39592
Fiona Ian <sayhitofiona@gmail.com>
2019-06-25 19:47:07 -0500

    added routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,16 @@
 class Api::ProductsController < ApplicationController
+  def first_product_method
+    @product1 = Product.first
+    render "first_product.json.jb"
+  end
+
+  def second_product_method
+    @product2 = Product.second
+    render "second_product.json.jb"
+  end
+
+  def third_product_method
+    @product3 = Product.third
+    render "third_product.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/first_product_url" => "products#first_product_method"
+    get "/second_product_url" => "products#second_product_method"
+    get "/third_product_url" => "products#third_product_method"
+  end
 end
