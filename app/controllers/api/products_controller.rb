e09768099d1993fcf6dca3a0525335ea21a7132e
Fiona Ian <sayhitofiona@gmail.com>
2019-06-25 20:15:15 -0500

    added all products route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,4 +1,9 @@
 class Api::ProductsController < ApplicationController
+  def all_products_method
+    @products = Product.all
+    render "all_products.json.jb"
+  end
+
   def first_product_method
     @product1 = Product.first
     render "first_product.json.jb"
@@ -12,5 +17,10 @@ class Api::ProductsController < ApplicationController
   def third_product_method
     @product3 = Product.third
     render "third_product.json.jb"
+  end
+
+  def fourth_product_method
+    @product4 = Product.fourth
+    render "fourth_product.json.jb"
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-Product.create(name: "La Croix Mango", price: 4, image_url: "https://i.ebayimg.com/images/g/0u0AAOSwfPlb2X0~/s-l300.jpg", description: "mango flavored sparkling water")
+Product.create(name: "Earl Grey Tea", price: 8, image_url: "https://www.englishteastore.com/media/catalog/product/cache/6/image/9df78eab33525d08d6e5fb8d27136e95/T/H/THAS_31603_-00_harney-and-sons-white-earl-grey-tea-tin-20-sachets.jpg", description: "black tea that tastes like orange and lemon")
+Product.create(name: "La Croix Mango", price: 4, image_url: "https://d2lnr5mha7bycj.cloudfront.net/product-image/file/large_5f6fa02b-6d34-4052-83ff-6493e8bcf313.png", description: "mango flavored sparkling water")
 Product.create(name: "Nutella", price: 10, image_url: "https://www.nutella.com/nutella-gl-theme/images/custom/Nutella.png", description: "hazelnut spread")
 Product.create(name: "Garrett Popcorn", price: 8, image_url: "https://cfcdn2.azsg.opensnap.com/azsg/snapphoto/photo/LB/GUFF/3BSDO2247D61C10C31F685lv.jpg", description: "caramel flavored popcorn")
